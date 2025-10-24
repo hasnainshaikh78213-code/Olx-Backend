9b838af8d29de700558050dbc5801be696554fac
Full Stack Developer <hasnainshaikh78213@gmail.com>
2025-10-24 14:15:53 -0700

    Update index.js

--- index.js
+++ index.js
@@ -62,6 +62,6 @@ app.get("/", (req, res) => {
 });
 
 //  Start Server
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`🚀 Server running on http://localhost:${PORT}`);
+// });
